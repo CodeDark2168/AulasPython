79bd3cbfe92ae3c0e88153e3c4b1c8339abf9d3e
Otávio Sudano <116005183+CodeDark2168@users.noreply.github.com>
2024-03-07 13:35:54 -0300

    Add files via upload

---
+++ Aula [03]/raioCirculo.py
@@ -0,0 +1,7 @@
+#Faça um codigo que leia do usuário o raio do círculo e calcule a área
+print("==============================")
+raio = float(input("Digite o valor do raio do círculo em centímetro: "))
+area = 3.1415*(raio*raio)
+aream= area/100
+print("A área do círculo é igual a " + str(area) + " centímetros ou " + str(aream) + " metros.")
+print("==============================")
