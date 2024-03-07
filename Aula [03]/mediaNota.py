575444cbfe8c97a34fb5b6dc151138ccdbf58816
Otávio Sudano <116005183+CodeDark2168@users.noreply.github.com>
2024-03-07 15:43:45 -0300

    Delete Aula [03] directory

--- Aula [03]/mediaNota.py
+++
@@ -1,8 +0,0 @@
-#Faça um programa que leia a nota 1, a nota 2 e calcule a média como media = nota1*0.5+nota2*0.5
-print("==============================")
-nota1 = float(input("Digite a primeira nota: "))
-nota2 = float(input("Digite a segunda nota: "))
-media = (nota1*0.5)+(nota2*0.5)
-print("==============================")
-print("Sua média é: " + str(media))
-print("==============================")
--- Aula [03]/temperatura.py
+++
@@ -1,6 +0,0 @@
-#Faça um programa que leia do usuário a temperatura em celcius e mostre em fahrenheit
-print("==============================")
-temperaturaC = int(input("Digite o valor da temperatura em celcius: "))
-temperaturaF = (temperaturaC*1.8)+32
-print("A temperatura digitada equivale a " + str(temperaturaF) + " Fahrenheit.")
-print("==============================")
